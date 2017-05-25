76033986821575ed7ab01862bb9107074f7fa839
Kurtis <krdgenius@gmail.com>
2017-05-24 20:56:54 -0400

    Basically Made the Nutrition Page
    Added cards for each macro and micro nutrient that is relevent (more to be added later) as well as one stickied card at the top for FDA guidelines to reading nutrition labels.

--- app/src/main/java/org/conner4real/kiwicare/NutritionFragment.java
+++ app/src/main/java/org/conner4real/kiwicare/NutritionFragment.java
@@ -1,16 +1,61 @@
 package org.conner4real.kiwicare;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class NutritionFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
+    //TODO: Set recommended amounts in a smart way
+
+    @BindView(R.id.nutritionLabelLink) TextView nutritionLabelLink;
+
+    @BindView(R.id.addAmountCalcium) EditText addAmountCalcium;
+    @BindView(R.id.addButtonCalcium) Button addButtonCalcium;
+    @BindView(R.id.addTotalCalcium) TextView addTotalCalcium;
+    private float calciumTotal = 0;
+    private int calciumRecommended = 1300;
+
+    @BindView(R.id.addAmountIron) EditText addAmountIron;
+    @BindView(R.id.addButtonIron) Button addButtonIron;
+    @BindView(R.id.addTotalIron) TextView addTotalIron;
+    private float ironTotal = 0;
+    private int ironRecommended = 11;
+
+    @BindView(R.id.addAmountMagnesium) EditText addAmountMagnesium;
+    @BindView(R.id.addButtonMagnesium) Button addButtonMagnesium;
+    @BindView(R.id.addTotalMagnesium) TextView addTotalMagnesium;
+    private float magnesiumTotal = 0;
+    private int magnesiumRecommended = 410;
+
+    @BindView(R.id.addAmountVitaminC) EditText addAmountVitaminC;
+    @BindView(R.id.addButtonVitaminC) Button addButtonVitaminC;
+    @BindView(R.id.addTotalVitaminC) TextView addTotalVitaminC;
+    private float vitaminCTotal = 0;
+    private int vitaminCRecommended = 75;
+
+    @BindView(R.id.addAmountVitaminA) EditText addAmountVitaminA;
+    @BindView(R.id.addButtonVitaminA) Button addButtonVitaminA;
+    @BindView(R.id.addTotalVitaminA) TextView addTotalVitaminA;
+    private float vitaminATotal = 0;
+    private int vitaminARecommended = 75;
 
     public NutritionFragment() {
         // Required empty public constructor
@@ -35,7 +80,127 @@ public class NutritionFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nutrition, container, false);
+        View view = inflater.inflate(R.layout.fragment_nutrition, container, false);
+        ButterKnife.bind(this, view);
+
+        //TODO: Use actual math from https://www.ncbi.nlm.nih.gov/books/NBK56068/table/summarytables.t3/?report=objectonly
+
+        String fdaLink = "https://www.fda.gov/food/ingredientspackaginglabeling/labelingnutrition/ucm274593.htm";
+
+        nutritionLabelLink.setMovementMethod(LinkMovementMethod.getInstance());
+        nutritionLabelLink.setText(Html.fromHtml("Here's the <a href=\"" + fdaLink + "\">official FDA guide</a> to reading nutrition labels on food."));
+
+        addButtonCalcium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int addedCalcium = Integer.parseInt(addAmountCalcium.getText().toString());
+                calciumTotal += addedCalcium;
+
+                float currentRatio = calciumTotal / calciumRecommended;
+
+                if (currentRatio <= 0.25 || currentRatio >= 1.25){
+                    addTotalCalcium.setTextColor(Color.RED);
+                }
+                else if (currentRatio <= 0.8 || currentRatio > 1){
+                    addTotalCalcium.setTextColor(Color.YELLOW);
+                }
+                else {
+                    addTotalCalcium.setTextColor(Color.GREEN);
+                }
+
+                addTotalCalcium.setText(calciumTotal + "/" + calciumRecommended + " mg");
+            }
+        });
+
+        addButtonIron.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int addedIron = Integer.parseInt(addAmountIron.getText().toString());
+                ironTotal += addedIron;
+
+                float currentRatio = ironTotal / ironRecommended;
+
+                if (currentRatio <= 0.25 || currentRatio >= 1.25){
+                    addTotalIron.setTextColor(Color.RED);
+                }
+                else if (currentRatio <= 0.8 || currentRatio > 1){
+                    addTotalIron.setTextColor(Color.YELLOW);
+                }
+                else {
+                    addTotalIron.setTextColor(Color.GREEN);
+                }
+
+                addTotalIron.setText(ironTotal + "/" + ironRecommended + " mg");
+            }
+        });
+
+        addButtonMagnesium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int addedMagnesium = Integer.parseInt(addAmountMagnesium.getText().toString());
+                magnesiumTotal += addedMagnesium;
+
+                float currentRatio = magnesiumTotal / magnesiumRecommended;
+
+                if (currentRatio <= 0.25 || currentRatio >= 1.25){
+                    addTotalMagnesium.setTextColor(Color.RED);
+                }
+                else if (currentRatio <= 0.8 || currentRatio > 1){
+                    addTotalMagnesium.setTextColor(Color.YELLOW);
+                }
+                else {
+                    addTotalMagnesium.setTextColor(Color.GREEN);
+                }
+
+                addTotalMagnesium.setText(magnesiumTotal + "/" + magnesiumRecommended + " mg");
+            }
+        });
+
+        addButtonVitaminC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int addedVitaminC = Integer.parseInt(addAmountVitaminC.getText().toString());
+                vitaminCTotal += addedVitaminC;
+
+                float currentRatio = vitaminCTotal / vitaminCRecommended;
+
+                if (currentRatio <= 0.25 || currentRatio >= 1.25){
+                    addTotalVitaminC.setTextColor(Color.RED);
+                }
+                else if (currentRatio <= 0.8 || currentRatio > 1){
+                    addTotalVitaminC.setTextColor(Color.YELLOW);
+                }
+                else {
+                    addTotalVitaminC.setTextColor(Color.GREEN);
+                }
+
+                addTotalVitaminC.setText(vitaminCTotal + "/" + vitaminCRecommended + " mg");
+            }
+        });
+
+        addButtonVitaminA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int addedVitaminA = Integer.parseInt(addAmountVitaminA.getText().toString());
+                vitaminATotal += addedVitaminA;
+
+                float currentRatio = vitaminATotal / vitaminARecommended;
+
+                if (currentRatio <= 0.25 || currentRatio >= 1.25){
+                    addTotalVitaminA.setTextColor(Color.RED);
+                }
+                else if (currentRatio <= 0.8 || currentRatio > 1){
+                    addTotalVitaminA.setTextColor(Color.YELLOW);
+                }
+                else {
+                    addTotalVitaminA.setTextColor(Color.GREEN);
+                }
+
+                addTotalVitaminA.setText(vitaminATotal + "/" + vitaminARecommended + " μg");
+            }
+        });
+
+        return view;
     }
 
     public void onButtonPressed(Uri uri) {
