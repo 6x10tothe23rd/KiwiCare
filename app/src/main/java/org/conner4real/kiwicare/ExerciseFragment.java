6fc8026786d3c9a300d4f45c5942dd5d430c26e3
Kurtis <krdgenius@gmail.com>
2017-05-26 13:18:47 -0400

    Exercise cards updated
    They now have a title, description and a rating for strength cardio and endurance. This feature is not complete yet.

--- app/src/main/java/org/conner4real/kiwicare/ExerciseFragment.java
+++ app/src/main/java/org/conner4real/kiwicare/ExerciseFragment.java
@@ -1,19 +1,33 @@
 package org.conner4real.kiwicare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 
 public class ExerciseFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
+
+    @BindView(R.id.plankCard) CardView plankCard;
+    //@BindView(R.id.pushupCard) CardView pushupCard;
+    //@BindView(R.id.situpCard) CardView situpCard;
+    //@BindView(R.id.runningCard) CardView runningCard;
+
+    String plankURL = "https://en.wikipedia.org/wiki/Plank_(exercise)";
+    ExerciseRatings plankRatings = new ExerciseRatings(0.5, 0.1, 1);
+    String pushupURL = "https://greatist.com/fitness/how-do-perfect-push";
+    String situpURL = "http://www.military.com/military-fitness/fitness-test-prep/proper-technique-for-curl-ups";
+    String runningURL = "http://www.fitnessmagazine.com/workout/running/running-101-a-beginners-guide/";
 
     public ExerciseFragment() {
         // Required empty public constructor
@@ -33,12 +47,49 @@ public class ExerciseFragment extends Fragment {
         }
     }
 
+    private void setRatings(ExerciseRatings ratings, View RatingsContainer){
+        //TODO: Actually do this, won't be that hard tbh
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_exercise, container, false);
 
         ButterKnife.bind(this, view);
+
+        View.OnClickListener clicker = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "";
+
+                if (v.equals(plankCard)){
+                    url = plankURL;
+                }
+                /*else if (v.equals(pushupCard)){
+                    url = pushupURL;
+                }
+                else if (v.equals(situpCard)){
+                    url = situpURL;
+                }
+                else if (v.equals(runningCard)){
+                    url = runningURL;
+                }*/
+                else {
+                    return;
+                }
+
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(url));
+                startActivity(i);
+            }
+        };
+
+        plankCard.setOnClickListener(clicker);
+        setRatings(plankRatings, plankCard.findViewById(R.id.ratingContainer));
+        //pushupCard.setOnClickListener(clicker);
+        //situpCard.setOnClickListener(clicker);
+        //runningCard.setOnClickListener(clicker);
 
         return view;
     }
---
+++ app/src/main/java/org/conner4real/kiwicare/ExerciseRatings.java
@@ -0,0 +1,25 @@
+package org.conner4real.kiwicare;
+
+public class ExerciseRatings {
+    private double m_strength;
+    private double m_cardio;
+    private double m_endurance;
+
+    public ExerciseRatings(double strength, double cardio, double endurance){
+        this.m_strength = strength;
+        this.m_cardio = cardio;
+        this.m_endurance = endurance;
+    }
+
+    public double get_strength() {
+        return m_strength;
+    }
+
+    public double get_cardio() {
+        return m_cardio;
+    }
+
+    public double get_endurance() {
+        return m_endurance;
+    }
+}
